39f58e45acb1c8596571d1e6d5a053b186e60a0a
hcni <hcni2@illinois.edu>
2024-12-03 10:46:54 -0600

    directly assign first value to true

--- mpiinfo.f90
+++ mpiinfo.f90
@@ -3,21 +3,16 @@ module mpiinfo
     implicit none
 
     integer :: intercomm
-    logical :: first
+    logical :: first = .true.
     
 contains
 
-subroutine mpiinfo_initializing()
-    first = .true.
-    
-end subroutine mpiinfo_initializing
+    subroutine mpiinfo_despawn()
+        integer :: ierr
+        call MPI_BCAST(.true., 1, MPI_LOGICAL, MPI_ROOT, intercomm, ierr)
+        call MPI_COMM_DISCONNECT(intercomm, ierr)
 
-subroutine mpiinfo_despawn()
-    integer :: ierr
-    call MPI_BCAST(.true., 1, MPI_LOGICAL, MPI_ROOT, intercomm, ierr)
-    call MPI_COMM_DISCONNECT(intercomm, ierr)
-
-    
-end subroutine mpiinfo_despawn
+        
+    end subroutine mpiinfo_despawn
     
 end module mpiinfo
